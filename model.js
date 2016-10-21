28abe5cd0a9bfaddba6a8037f7b018bccabdb4d3
Philippe Assis <assis@philippeassis.com>
2016-10-21 16:54:29 -0200

    all

--- model.js
+++ model.js
@@ -5,15 +5,17 @@ var Types = keystone.Field.Types;
  * User Model
  * ==========
  */
-var Config = new keystone.List('Config');
+module.exports = () => {
+	var Config = new keystone.List('Config');
 
-Config.add({
-	meta: { type: String, initial: true, required: true },
-	tag: { type: String, initial: true, required: true },
-	value: { type: String, initial: true, required: true }
-});
+	Config.add({
+		meta: { type: String, initial: true, required: true },
+		tag: { type: String, initial: true, required: true },
+		value: { type: String, initial: true, required: true }
+	});
 
 
-Config.defaultColumns = 'meta, tag, value';
+	Config.defaultColumns = 'meta, tag, value';
 
-Config.register();
+	Config.register();
+}
